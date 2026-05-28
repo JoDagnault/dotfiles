78de409c1e1aff499184aa1a24f83dee88c780df
JoDagnault <jdagnault@gmail.com>
2026-05-28 19:12:37 -0400

    Add hyprland focus keybinds

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -28,3 +28,9 @@ hl.bind(mainMod .. " + C", hl.dsp.window.close())
 hl.bind(mainMod .. " + M", hl.dsp.exit())
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+
+-- Focus
+hl.bind(mainMod .. " + h", hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + j", hl.dsp.focus({ direction = "down" }))
+hl.bind(mainMod .. " + k", hl.dsp.focus({ direction = "up" }))
+hl.bind(mainMod .. " + l", hl.dsp.focus({ direction = "right" }))
