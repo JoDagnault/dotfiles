90aa5e6de0fce1da966d2ad8b444708517f91c64
JoDagnault <jdagnault@gmail.com>
2026-06-12 00:56:26 -0400

    Add lsp config

---
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      vim.lsp.config('lua_ls', {
+          settings = {
+              Lua = {
+                  diagnostics = {
+                      globals = { "vim", "it", "describe", "before_each", "after_each" },
+                  },
+              },
+          },
+      })
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "omnisharp"
+      },
+        automatic_enable = true,
+      })
+    end,
+  },
+}
